e849329930ef84294d1e29f77e6cef6cd8ae0a2c
Jeroen Heijmans <github@jeroenheijmans.nl>
2026-04-11 18:01:53 +0200

    ♻️ refactor: move navbar components to subfolder

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import Navbar from '$lib/components/Navbar.svelte';
+	import Navbar from '$lib/components/navbar/Navbar.svelte';
 
 	let { children, data } = $props();
 </script>
